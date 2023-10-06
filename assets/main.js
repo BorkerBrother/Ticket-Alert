6328f8c16c012de4d7d166e7df93cce5cc40c85d
KBorker <kb507300@fh-muenster.de>
2023-10-06 14:43:48 +0200

    Next Step Get Ticket ID

--- assets/main.js
+++ assets/main.js
@@ -19,32 +19,42 @@
         }
       );
 
+    // Ticket Info Custom Field 
+    client.get('ticket.customField:custom_field_19134886927633').then(
+      function(data) {
+        var ticket_info = data['ticket.customField:custom_field_19134886927633'];
+        console.log(ticket_info);
+        //requestTicketInfo(client, ticket_info);
+      }
+    );
 
-    // GET TICKET INFO as request
-    client.request('/api/v2/tickets/recent').then(
+    // Get Ticket_id
+    client.get('ticket.id').then(function(data)
+    {
+      console.log(data);
+      var id = jsonObj.getInt("ticket.id")
+      console.log(id);
+    })
+
+    // GET TICKET INFO as request ( Only for data)
+    client.request('/api/v2/tickets/'+ 1).then(
       function(tickets) {
-        console.log(tickets);
+        //console.log(tickets);
       },
       function(response) {
         console.error(response.responseText);
       }
     );
 
-      // Ticket info as get
+      // Ticket info as get (Only for data)
     client.get('ticket').then(
       function(data) {
         var user_id = data['ticket'];
-        console.log(user_id);
+        //console.log(user_id);
       }
     );
 
-    // Ticket Info Custom Field 
-    client.get('ticket.customField:custom_field_19134886927633').then(
-      function(data) {
-        var ticket_info = data['ticket.customField:custom_field_19134886927633'];
-        console.log(ticket_info);
-      }
-    );
+    
 
   
 
@@ -55,14 +65,15 @@
 function requestTicketInfo(client, id) {
 
   var settings = {
-    url: '/api/v2/tickets/' + id + '.json',
+    url: '/api/v2/tickets' + id + '.json', // 
     type:'GET',
     dataType: 'json',
   };
 
   client.request(settings).then(
     function(data) {
-      showInfo(data);
+      console.log(data);
+      //showInfoTicket(data);
     },
     function(response) {
       showError(response);
@@ -71,18 +82,18 @@ function requestTicketInfo(client, id) {
 }
 
 ///// SHOW USER INFO
-  function requestUserInfo(client, id) {
+  function requestUserInfo(client, user_id) {
 
     var settings = {
-      url: '/api/v2/users/' + id + '.json',
+      url: '/api/v2/users/' + user_id + '.json',
       type:'GET',
       dataType: 'json',
     };
   
     client.request(settings).then(
       function(data) {
-        showInfo(data);
-        console.log(data);
+        showInfoUser(data);
+        //console.log(data);
       },
       function(response) {
         showError(response);
@@ -92,12 +103,28 @@ function requestTicketInfo(client, id) {
   
 
 // Show Info User
-  function showInfo(data) {
+  function showInfoUser(data) {
     var requester_data = {
       'name': data.user.name,
       'tags': data.user.tags,
-      'created_at': formatDate(data.user.created_at), // should be ticket info
+      //'created_at': formatDate(data.user.created_at), // should be ticket info
       //'finish_at': formatDate(data.user.finish_at) //     should be ticket info
+    };
+    
+
+    var source = document.getElementById("requester-template").innerHTML; // Get template from html
+    var template = Handlebars.compile(source);                            // 
+    var html = template(requester_data);
+    document.getElementById("content").innerHTML = html;
+  }
+
+  // Show Info Ticket
+  function showInfoTicket(data) {
+    var requester_data = {
+      //'name': data.user.name,
+      //'tags': data.user.tags,
+      //'created_at': formatDate(data.user.created_at), // should be ticket info
+      'finish_at': data.ticket//     should be ticket info
     };
   
     var source = document.getElementById("requester-template").innerHTML;
