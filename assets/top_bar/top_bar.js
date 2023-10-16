3dca9b362ef2e0673809a4a4fc64bd29dc9d593a
KBorker <borkerkai@yahoo.de>
2023-10-16 16:15:07 +0200

    Update Top Bar - Issue: Get custom ticket value

--- assets/top_bar/top_bar.js
+++ assets/top_bar/top_bar.js
@@ -2,40 +2,11 @@
 (function () {
 
     var client = ZAFClient.init();
-  
-  
-    // Starte die Überprüfungsfunktion beim Laden der App 
-    checkAndUpdateTicketStatus(client);
+
+    checkAndUpdateAllTickets(client);
+    // Solange Ticket 
+    checkAllTicketsForExpiry(client);
     
-  
-      // Get Ticket_id
-    client.get('ticket.id').then(function(data) {
-      //console.log(data);
-    
-    var ticketId;
-    
-    // GET TICKET INFO as request 
-    client.request('/api/v2/tickets/' + ticketId).then(
-      function(tickets) {
-        console.log(tickets);
-  
-        client.get('ticket.customField:custom_field_19134886927633').then(function(data) {
-          // Datum aus dem benutzerdefinierten Feld abrufen
-          var customFieldDate = data['ticket.customField:custom_field_19134886927633'];
-  
-          // Das Datum in einen String umwandeln 
-          var formattedDate = formatDate(customFieldDate);
-  
-          showInfoTicket(formattedDate);
-          
-        });
-      },
-      function(response) {
-        console.error(response.responseText);
-      }
-    );
-    });
-  
   })();
   
 
@@ -55,12 +26,15 @@ function checkAllTicketsForExpiry(client) {
     // Rufe alle Tickets ab
     client.request('/api/v2/tickets.json').then(function(response) {
       const tickets = response.tickets;
-      console.log(response);
       
       // Durchlaufe alle Tickets und überprüfe das Ablaufdatum
       tickets.forEach(function(ticket) {
-        const customFieldDate = new Date(ticket.custom_fields.find(field => field.id === 'custom_field_19134886927633').value);
-  
+        console.log(ticket);
+
+        const customFieldDate = ticket.custom_fields.find(field => field.id === 'custom_field_19134886927633').value;
+
+        console.log(customFieldDate2);
+
         if (isDateExpired(customFieldDate)) {
           // Das Datum ist abgelaufen, ändere den Ticketstatus hier
           changeTicketStatus(client, ticket.id, 'new'); // Du kannst hier den gewünschten Status verwenden
@@ -68,38 +42,9 @@ function checkAllTicketsForExpiry(client) {
       });
     });
   }
+
   
-  
-  // Funktion zum Ändern des Ticketstatus
-  
-  
-  // Funktion zum Überprüfen und Aktualisieren des Ticketstatus im Ticket 
-  function checkAndUpdateTicketStatus(client) {
-    // Hole das aktuelle Ticket
-    var ticketId;
-    // Get Ticket_id
-    client.get('ticket.id').then(function(data) {
-    //console.log(data);
-        
-    // Die Ticket-ID als Integer abrufen
-    ticketId = parseInt(data['ticket.id']);
-  
-      // Hole das Datum aus dem benutzerdefinierten Feld
-      client.get('ticket.customField:custom_field_19134886927633').then(function(data) {
-        var customFieldDate = data['ticket.customField:custom_field_19134886927633'];
-  
-        // Konvertiere das Datum in ein Date-Objekt
-        var dateToCheck = new Date(customFieldDate);
-  
-        // Überprüfe, ob das Datum abgelaufen ist
-        if (isDateExpired(dateToCheck)) {
-          // Das Datum ist abgelaufen, ändere den Ticketstatus hier
-          changeTicketStatus(client, ticketId, 'new'); // Du kannst hier den gewünschten Status verwenden
-        }
-      });
-    });
-  }
-  
+
   
   
   // Hier wird die Funktion zur Überprüfung des Ticketstatus aufgerufen
